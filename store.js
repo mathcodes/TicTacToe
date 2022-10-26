76b2196ee0db616382e979e47ccf4ea8d0a86245
Jon Christie <jonpchristie@gmail.com>
2022-10-26 07:53:42 +0000

    Initial commit

---
+++ store.js
@@ -0,0 +1,40 @@
+var subscribers = [];
+var middlewares;
+
+function Store(mid) {
+	middlewares = mid || [];
+
+	
+}
+
+// prototype.getState
+
+// prototype.getPrevState
+
+// prototype.getPrevState
+
+// prototype._combineMiddlewares
+
+// prototype.reduce
+
+// prototype.subscribe
+
+// prototype.Notifysubscribers
+
+// Functions:
+
+function updateGrid
+
+function updateCell
+
+function updateTurn
+
+function updateScore
+    
+function updateWinnerSequence
+
+function updateCounter
+
+function updatePlayer
+
+module.exports = Store;
